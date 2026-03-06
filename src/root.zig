16c6571a1ed37af66ac58b4999b17ce85d6f6b04
jonk <jonk@jonk.com>
2026-03-06 08:46:22 -0500

    add exports to root

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+pub const Vec2 = @import("vec2.zig").Vec2;
+pub const Rect = @import("rect.zig").Rect;
+
 test {
     _ = @import("vec2.zig");
     _ = @import("rect.zig");
